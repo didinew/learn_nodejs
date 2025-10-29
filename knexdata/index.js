31d882688c92c56c3ba7edd1739458c4ea6a178a
di <1426567242@qq.com>
2025-10-29 22:46:25 +0800

    knex

--- knexdata/index.js
+++ knexdata/index.js
@@ -1,12 +1,45 @@
 import knex from 'knex';
 import fs from 'fs';
+import jsyaml from 'js-yaml';
+import express from 'express';
 
+const app = express();
+const port = 3000;
 
-const config = fs.readFileSync('./db.config.yaml', 'utf8');
-console.log('Database config loaded:', config.db);
+app.use(express.json());
+
+const yaml = fs.readFileSync('./db.config.yaml', 'utf8');
+const config = jsyaml.load(yaml);
 
 const db = knex({
   client: 'mysql2',
   connection: config.db
 });
 
+
+// insert 新的数据
+app.post('/create', async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const [id] = await db('users').insert({ name, email });
+    res.status(201).json({ id, name, email });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+// get 所有的数据
+app.get('/list', async (req, res) => {
+  try {
+    const users = await db('users').select('*').orderBy('id', 'desc');
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
